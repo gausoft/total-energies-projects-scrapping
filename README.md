e664a1a9a7ea005c672308f3a9e54410ae39a10f
Gauthier EHOLOUM <thiergau691@gmail.com>
2022-01-25 11:49:20 +0000

    chore: Refactoring

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Total Energies Startups Scrapping
+## [Total Energies Startups](https://startupper.totalenergies.com/en) Scrapping
 
 This PHP project currently scrapes only project from Togo.
 
@@ -10,20 +10,34 @@ Using [symfony panther](https://github.com/symfony/panther) package as proof of 
 git clone https://github.com/gausoft/total-energies-projects-scrapping.git
 ```
 
+```
+cd total-energies-projects-scrapping/
+```
+
 ```bash
 composer install
 ```
 
-## Launch
+## Install a headless browser
 
 ```bash
-php startupers.php
+composer require --dev dbrekelmans/bdi
+vendor/bin/bdi detect drivers
 ```
+
+## Launch Scrapping
+
+```bash
+php src/startupers.php
+```
+
+Data are exported to a csv file under `data/` directory
+
 
 
 ### TODO
-[] Add support for other country 
+[] Add support for other countries 
 
-[] Scrape from a details page using js
+[] Scrape data without loading pages
 
-[] Refactoring
+[x] Refactoring
--- src/startuppers.php
+++ src/startuppers.php
@@ -1,31 +1,30 @@
 <?php
 
-use Symfony\Component\Panther\Client;
+$rootDir = dirname(dirname(__FILE__));
 
-require __DIR__ . '/vendor/autoload.php'; // Composer's autoloader
+require $rootDir . '/vendor/autoload.php'; // Composer's autoloader
+
+use Symfony\Component\Panther\Client;
 
 $client = Client::createFirefoxClient();
 
 $currentPage = 1;
 $lastPage = 2;
 
-$url = "https://startupper.totalenergies.com/en/juries/latC_MEsLWQo_ndWzJ-OJg?query=&order=alphabetical&scope=all";
-
-$csvFile = fopen('startuppers.csv', 'w');
-
-fputcsv($csvFile, ['Projet', 'Image', 'Initiateur', 'Description']); //header
+$url = "https://startupper.totalenergies.com/fr/juries/latC_MEsLWQo_ndWzJ-OJg?query=&order=alphabetical&scope=all";
 
 try {
 
-    $client->request('GET', $url . "&p=$currentPage"); // Yes, this website is 100% written in JavaScript
+    $client->request('GET', $url . "&p=$currentPage");
 
     $crawler = $client->waitForVisibility('#pagination-container');
 
     echo $crawler->filter('.slogan-challenge > h1')->text() . "\n";
 
-    if ($currentPage == 1) {
-        $lastPage = getLastPageNumber($crawler);
-    }
+    $lastPage = getLastPageNumber($crawler);
+
+    $csvFile = fopen($rootDir . '/data/startuppers.csv', 'w');
+    fputcsv($csvFile, ['Projet', 'Image', 'Promoteur', 'Description']); //CSV header
 
     while ($currentPage <= $lastPage) {
         print "Scrapping page : $currentPage  \n";
@@ -33,7 +32,8 @@ try {
         $crawler->filter('.super-card')->each(function ($node) use ($csvFile) {
             $nodeStyle = $node->filter('.bg')->attr('style');
 
-            preg_match('/(http|https):\/\/([A-Z0-9][A-Z0-9_-]*(?:.[A-Z0-9][A-Z0-9_-]*)+):?(d+)?\/?/i', $nodeStyle, $match);
+            $urlPattern = "(http|https):\/\/([A-Z0-9][A-Z0-9_-]*(?:.[A-Z0-9][A-Z0-9_-]*)+):?(d+)?\/?";
+            preg_match("/$urlPattern/i", $nodeStyle, $match); //Extract image url from element style
 
             if (empty($match)) {
                 preg_match('/team-avatar-\d+.jpg/', $nodeStyle, $match);
@@ -48,7 +48,7 @@ try {
 
             fputcsv($csvFile, [$projectName, $logoUrl, $promoter, $description]);
         });
-        
+
         $currentPage++;
 
         $client->request('GET', $url . "&p=$currentPage");
@@ -58,7 +58,7 @@ try {
 
     fclose($csvFile);
 
-    echo "Scripping finished successfully!";
+    echo "Scrapping finished successfully! \n";
 } catch (Exception $e) {
     echo $e->getMessage();
 } finally {
